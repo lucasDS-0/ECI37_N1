33d2219a6f400bac234f89192cc852d3e10b5aa2
Lucas <90753591+lucasDS-0@users.noreply.github.com>
2024-08-24 17:16:39 -0300

    segunda iteracion parsers monadicos

--- src/Ejercicio3c.hs
+++ src/Ejercicio3c.hs
@@ -10,51 +10,51 @@ data UProp = Or UProp UProp
            | Num Int
 
 prop_parser :: Parser UProp
-prop_parser = do term <- term_parser          -- term \/ prop
-                 pSmy '\'
+prop_parser = do term <- term_parser             -- term \/ prop
+                 pSym '\'
                  pSym '/'
                  prop <- prop_parser
                  return $ Or term prop
               <|>
-              do term <- term_parser
+              do term <- term_parser             -- term
                  return term
 
 term_parser :: Parser UProp 
-term_parser = do factor <- factor_parser      -- factor /\ term
-                 pSmy '/'
+term_parser = do factor <- factor_parser         -- factor /\ term
+                 pSym '/'
                  pSym '\'
                  term <- term_parser
                  return $ And factor term  
               <|>
-              do factor <- factor_parser
+              do factor <- factor_parser         -- factor
                  return factor
 
 factor_parser :: Parser UProp
-factor_parser = do pSym '~'                   -- ~ prop  
-                 prop <- prop_parser
-                 return $ Neg prop
-              <|>
-              do pSym '('                     -- (prop)
-                  prop <- prop_parser
-                  pSym ')'
-                  return $ PA prop
-              <|>
-              do pSym '('                     -- (prop1 = prop2)
-                 prop1 <- prop_parser
-                 pSym '='
-                 prop2 <- prop_parser
-                 pSym ')'
-                 return $ EQ prop1 prop2
-              <|>
-              do pSym '('                     -- (prop1 < prop2)
-                 prop1 <- prop_parser
-                 pSym '='
-                 prop2 <- prop_parser
-                 pSym ')'
-                 return $ EQ prop1 prop2
-              <|>
-              do n <- number                  -- N
-                 return $ Num n
+factor_parser = do pSym '~'                      -- ~ prop  
+                   prop <- prop_parser
+                   return $ Neg prop
+                <|>
+                do pSym '('                      -- (prop)
+                   prop <- prop_parser
+                   pSym ')'
+                   return $ PA prop
+                <|>
+                do pSym '('                      -- (prop1 = prop2)
+                   prop1 <- prop_parser
+                   pSym '='
+                   prop2 <- prop_parser
+                   pSym ')'
+                   return $ EQ prop1 prop2
+                <|>
+                do pSym '('                      -- (prop1 < prop2)
+                   prop1 <- prop_parser
+                   pSym '='
+                   prop2 <- prop_parser
+                   pSym ')'
+                   return $ EQ prop1 prop2
+                <|>
+                do n <- number                   -- N
+                   return $ Num n
 
 
 
